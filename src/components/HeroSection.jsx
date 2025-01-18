e425e385856dd9a701f5cca45afd6b141b340c2f
khansahabas03 <khansahabas03@gmail.com>
2025-01-19 00:04:55 +0530

    Navbar not change

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <section id="hero">
             <div className="hero-content">
                 <h2>Hello, I'm Sahabas Khan</h2>
-                <p>Fresher | B.Tech in Computer Science | Full Stack Developer</p>
+                <p>Fresher | B.Tech in Computer Science | Full Stack Java Developer</p>
                 <a href="#contact" id='touch'>Get in Touch</a>
             </div>
             <div className="hero-image">
